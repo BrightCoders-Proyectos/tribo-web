86ef1593f3a965e4c3bf2b5a31ea6383abcbd17e
EduJimenez96 <ejimenez10@ucol.mx>
2020-12-15 13:34:19 -0600

    sigin by phone instead email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < Clearance::UsersController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :phone, :email, :password)
   end
 end
