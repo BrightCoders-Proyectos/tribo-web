5f514058778c320d8671cbe2eace36182d49a7e6
KarinaDell <36238027+KarinaDell@users.noreply.github.com>
2020-12-15 18:10:38 -0600

    Delete database.yml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < Clearance::UsersController
   private
 
+<<<<<<< HEAD
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+=======
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+>>>>>>> db77c1c... Delete database.yml
 end
