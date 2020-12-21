d017577415e10845fd0fdea2f46db53f4f5d7d5c
EduJimenez96 <ejimenez10@ucol.mx>
2020-12-21 12:44:36 -0600

    fixup! Merge branch 'master' into feature#4-db-setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,7 @@
 class UsersController < Clearance::UsersController
   private
 
-<<<<<<< HEAD
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-=======
-
-    private
-
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
->>>>>>> db77c1c... Delete database.yml
 end
