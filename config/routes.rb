f4e3154e5f0d8f4e293552e4b88e2c5bc6755d97
EduJimenez96 <ejimenez10@ucol.mx>
2021-01-06 17:56:02 -0600

    fix root on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   resources :market_places
   get 'main/dashboard'
+  
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
