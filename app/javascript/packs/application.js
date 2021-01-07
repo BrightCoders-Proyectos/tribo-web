6628de34ba588fcf4c99ab421de1221c64f9ee56
ediazjz <ediazjz@outlook.com>
2021-01-07 11:07:08 -0600

    Add Font Awesome
    Font Awesome was added using Yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels")
 
 // import "tailwindcss/tailwind.css"
 import "stylesheets/application"
+// Import Font Awesome Icons
+import "@fortawesome/fontawesome-free/css/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
