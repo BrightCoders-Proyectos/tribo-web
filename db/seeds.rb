3aff12207c8697550eda6297a89beeba3819f85b
Karina Delgado <kdelgado@ucol.mx>
2021-01-14 18:33:15 -0600

    Added modal structure
    Co-authored-by: EduJimenez96 <ejimenez10@ucol.mx>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MarketPlace.create(user_id: 1, business_name: 'Tacos El Unico', address: 'Av. Sevilla del Rio 455', 
+district: 'Colima', postcode: 28970, business_line: 'Food', pickup_type: 'Servicio a domicilio', latitud: 19.254087, longitud: -103.714371,
+open_hour: '19:00', close_hour: '00:00', service_days: "Lunes - Domingo")
