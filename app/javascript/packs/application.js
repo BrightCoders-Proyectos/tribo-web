6aed71270a1398b2706e24a6043d90ffd1865b3c
Ricardo <cardotrejos@gmail.com>
2021-01-22 10:58:26 -0500

    finish precommits setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,21 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
 // import "tailwindcss/tailwind.css"
-import "stylesheets/application"
+import 'stylesheets/application';
 // Import Font Awesome Icons
-import "@fortawesome/fontawesome-free/css/all"
+import '@fortawesome/fontawesome-free/css/all';
 
-import "controllers"
+import 'controllers';
+
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
 
 window.dispatchMapsEvent = function (...args) {
-  const event = document.createEvent("Events")
-  event.initEvent("google-maps-callback", true, true)
-  event.args = args
-  window.dispatchEvent(event)
-}
+  const event = document.createEvent('Events');
+  event.initEvent('google-maps-callback', true, true);
+  event.args = args;
+  window.dispatchEvent(event);
+};
