ea0472d26a9e10c1f688fa762561241dbac84939
Karina Delgado <kdelgado@ucol.mx>
2021-01-26 14:27:36 -0600

    My account view moved to a path
    Co-authored-by: EduJimenez96 <ejimenez10@ucol.mx>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
-class UsersController < Clearance::UsersController
-  private
+# frozen_string_literal: true
 
-  def user_params
-    params.require(:user).permit(:name, :phone, :email, :password)
+class UsersController < Clearance::UsersController
+  before_action :current_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :phone, :email, :password)
+    end
+
+    def current_user
+      @user = User.find(params[:id])
+    end
 end
